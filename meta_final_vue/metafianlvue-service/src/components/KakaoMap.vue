c807c3bd95b8edc88131d527f03e61a4fafc0ff5
jeonga-Son <jjeong0106@gmail.com>
2023-04-10 23:09:34 +0900

    [feat] add zoom_changed feature

--- meta_final_vue/metafianlvue-service/src/components/KakaoMap.vue
+++ meta_final_vue/metafianlvue-service/src/components/KakaoMap.vue
@@ -177,6 +177,10 @@ export default {
 
           console.log(message);
       });
+
+      // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+      var zoomControl = new kakao.maps.ZoomControl();
+      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     }
   }
 }
